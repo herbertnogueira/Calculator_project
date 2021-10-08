9cb97a3e77f7b41d16e175568b8cb2f46338cb8f
herbertnogueira <herbertdossantos15@gmail.com>
2021-10-08 20:16:22 -0300

    Finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,31 @@
 function calcular(n1, n2){
-n1 = document.getElementById("n1").value
-n2 = document.getElementById("n2").value
+n1 = parseInt(document.getElementById("n1").value)
+n2 = parseInt(document.getElementById("n2").value)
+
+seletor = document.getElementById("seletor").value
+
+switch(seletor){
+    case '+':
+        calculo = n1+n2
+        document.getElementById("resultado").innerHTML = "Soma igual a "+ calculo
+    break
+    
+    case '-':
+    calculo = n1-n2
+    document.getElementById("resultado").innerHTML = "Subtração igual a " +calculo
+    break
+
+    case '*':
+        calculo = n1*n2
+        document.getElementById("resultado").innerHTML = "Multiplicação igual a " +calculo
+    break
+
+    case '/':
+        calculo = n1/n2
+        document.getElementById("resultado").innerHTML = "Divisão igual a " +calculo
+        break
+
+        default:
+        document.getElementById("resultado").innerHTML = "insira dados válidos"
+    }
 }
